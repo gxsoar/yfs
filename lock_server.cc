8030074ab6d2a561354aa6d328fa2592895d18d9
gxsoar <2278926493gx@gmail.com>
2023-01-14 21:59:02 +0800

    add accquire and release

--- lock_server.cc
+++ lock_server.cc
@@ -1,23 +1,51 @@
 // the lock server implementation
 
 #include "lock_server.h"
-#include <sstream>
+
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 
-lock_server::lock_server():
-  nacquire (0)
-{
-}
+#include <sstream>
 
-lock_protocol::status
-lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
-{
+lock_server::lock_server() : nacquire(0) {}
+
+lock_protocol::status lock_server::stat(int clt, lock_protocol::lockid_t lid,
+                                        int &r) {
   lock_protocol::status ret = lock_protocol::OK;
   printf("stat request from clt %d\n", clt);
   r = nacquire;
   return ret;
 }
 
+int lock_server::accquire(int clt, lock_protocol::lockid_t lid, int &r) {
+  lock_protocol::status ret = lock_protocol::OK;
+  auto ite = lock_tale_.find(lid);
+  std::unique_lock<std::mutex> ulock(mutex_);
+  if (ite != lock_tale_.end()) {
+    auto the_lock = ite->second;
+    while(the_lock->lock_state_ != LockState::FREE) {
+      the_lock->cv_.wait(ulock);
+    }
+    the_lock->lock_state_ = LockState::LOCKED;
+  } else {
+    lock *new_lock = new lock(lid);
+    new_lock->lock_state_ = LockState::LOCKED;
+    lock_tale_[lid] = new_lock;
+  }
+  return ret;  
+}
 
+int lock_server::release(int clt, lock_protocol::lockid_t lid, int &r) {
+  std::unique_lock<std::mutex> ulock(mutex_);
+  lock_protocol::status ret = lock_protocol::OK;
+  auto ite = lock_tale_.find(lid);
+  if (ite == lock_tale_.end()) {
+    ret = lock_protocol::IOERR;
+    return ret;
+  }
+  auto the_lock = lock_tale_[lid];
+  the_lock->lock_state_ = LockState::FREE;
+  the_lock->cv_.notify_all();
+  return ret; 
+}
--- lock_server.h
+++ lock_server.h
@@ -5,26 +5,25 @@
 #define lock_server_h
 
 #include <string>
-#include "lock_protocol.h"
+#include <map>
+#include <unordered_map>
+#include <mutex>
+
 #include "lock_client.h"
+#include "lock_protocol.h"
 #include "rpc.h"
 
 class lock_server {
-
  protected:
   int nacquire;
-
+  std::unordered_map<lock_protocol::lockid_t, lock*> lock_tale_;
+  std::mutex mutex_;
  public:
   lock_server();
-  ~lock_server() {};
+  ~lock_server(){};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  int accquire(int clt, lock_protocol::lockid_t lid, int &);
+  int release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
-#endif 
-
-
-
-
-
-
-
+#endif

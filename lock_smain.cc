a12c6deef68160d355679a7a6a28fd63d614c4db
gxsoar <2278926493gx@gmail.com>
2023-01-14 21:57:27 +0800

    reg lock_server accquire & release

--- lock_smain.cc
+++ lock_smain.cc
@@ -1,22 +1,17 @@
-#include "rpc.h"
 #include <arpa/inet.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <signal.h>
-#include "lock_server.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "jsl_log.h"
+#include "lock_server.h"
+#include "rpc.h"
 
 // Main loop of lock_server
 
-static void
-force_exit(int) {
-    exit(0);
-}
+static void force_exit(int) { exit(0); }
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   int count = 0;
 
   // Force the lock_server to exit after 20 minutes
@@ -28,25 +23,25 @@ main(int argc, char *argv[])
 
   srandom(getpid());
 
-  if(argc != 2){
+  if (argc != 2) {
     fprintf(stderr, "Usage: %s port\n", argv[0]);
     exit(1);
   }
 
   char *count_env = getenv("RPC_COUNT");
-  if(count_env != NULL){
+  if (count_env != NULL) {
     count = atoi(count_env);
   }
 
-  //jsl_set_debug(2);
+  // jsl_set_debug(2);
 
 #ifndef RSM
   lock_server ls;
   rpcs server(atoi(argv[1]), count);
   server.reg(lock_protocol::stat, &ls, &lock_server::stat);
+  server.reg(lock_protocol::acquire, &ls, &lock_server::accquire);
+  server.reg(lock_protocol::release, &ls, &lock_server::release);
 #endif
 
-
-  while(1)
-    sleep(1000);
+  while (1) sleep(1000);
 }

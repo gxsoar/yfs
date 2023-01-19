3953f0c55f4a31fe40963b652835d7db7e86d248
gxsoar <2278926493@qq.com>
2023-01-19 14:38:38 +0800

    update extent_server yfs_client

--- extent_server.cc
+++ extent_server.cc
@@ -1,18 +1,18 @@
 // the extent server implementation
 
-#include <sstream>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include "extent_server.h"
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <sstream>
 
 extent_server::extent_server() {}
 
-
-int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &)
-{
+int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &) {
   // You fill this in for Lab 2.
   std::scoped_lock<std::mutex> lock(mutex_);
   if (extent_content_.count(id) || extent_attr_.count(id)) {
@@ -28,8 +28,7 @@ int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &)
   return extent_protocol::OK;
 }
 
-int extent_server::get(extent_protocol::extentid_t id, std::string &buf)
-{
+int extent_server::get(extent_protocol::extentid_t id, std::string &buf) {
   // You fill this in for Lab 2.
   std::scoped_lock<std::mutex> lock(mutex_);
   if (extent_content_.count(id) == 0U || extent_attr_.count(id) == 0U) {
@@ -40,8 +39,8 @@ int extent_server::get(extent_protocol::extentid_t id, std::string &buf)
   return extent_protocol::OK;
 }
 
-int extent_server::getattr(extent_protocol::extentid_t id, extent_protocol::attr &a)
-{
+int extent_server::getattr(extent_protocol::extentid_t id,
+                           extent_protocol::attr &a) {
   // You fill this in for Lab 2.
   // You replace this with a real implementation. We send a phony response
   // for now because it's difficult to get FUSE to do anything (including
@@ -60,8 +59,7 @@ int extent_server::getattr(extent_protocol::extentid_t id, extent_protocol::attr
   return extent_protocol::OK;
 }
 
-int extent_server::remove(extent_protocol::extentid_t id, int &)
-{
+int extent_server::remove(extent_protocol::extentid_t id, int &) {
   // You fill this in for Lab 2.
   if (extent_content_.count(id) == 0 || extent_attr_.count(id) == 0U) {
     return extent_protocol::IOERR;
@@ -70,4 +68,3 @@ int extent_server::remove(extent_protocol::extentid_t id, int &)
   extent_attr_.erase(id);
   return extent_protocol::OK;
 }
-
--- yfs_client.cc
+++ yfs_client.cc
@@ -1,55 +1,42 @@
 // yfs client.  implements FS operations using extent and lock server
-#include "extent_client.h"
-#include <sstream>
-#include <iostream>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include "yfs_client.h"
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-yfs_client::yfs_client(std::string extent_dst, std::string lock_dst)
-{
+#include <iostream>
+#include <sstream>
+
+#include "extent_client.h"
+
+yfs_client::yfs_client(std::string extent_dst, std::string lock_dst) {
   ec = new extent_client(extent_dst);
-
 }
 
-yfs_client::inum
-yfs_client::n2i(std::string n)
-{
+yfs_client::inum yfs_client::n2i(std::string n) {
   std::istringstream ist(n);
   unsigned long long finum;
   ist >> finum;
   return finum;
 }
 
-std::string
-yfs_client::filename(inum inum)
-{
+std::string yfs_client::filename(inum inum) {
   std::ostringstream ost;
   ost << inum;
   return ost.str();
 }
 
-bool
-yfs_client::isfile(inum inum)
-{
-  if(inum & 0x80000000)
-    return true;
+bool yfs_client::isfile(inum inum) {
+  if (inum & 0x80000000) return true;
   return false;
 }
 
-bool
-yfs_client::isdir(inum inum)
-{
-  return ! isfile(inum);
-}
+bool yfs_client::isdir(inum inum) { return !isfile(inum); }
 
-int
-yfs_client::getfile(inum inum, fileinfo &fin)
-{
+int yfs_client::getfile(inum inum, fileinfo &fin) {
   int r = OK;
 
   printf("getfile %016llx\n", inum);
@@ -65,14 +52,12 @@ yfs_client::getfile(inum inum, fileinfo &fin)
   fin.size = a.size;
   printf("getfile %016llx -> sz %llu\n", inum, fin.size);
 
- release:
+release:
 
   return r;
 }
 
-int
-yfs_client::getdir(inum inum, dirinfo &din)
-{
+int yfs_client::getdir(inum inum, dirinfo &din) {
   int r = OK;
 
   printf("getdir %016llx\n", inum);
@@ -85,9 +70,6 @@ yfs_client::getdir(inum inum, dirinfo &din)
   din.mtime = a.mtime;
   din.ctime = a.ctime;
 
- release:
+release:
   return r;
 }
-
-
-

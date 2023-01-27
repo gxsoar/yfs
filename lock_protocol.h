3583b787ca45bc2aec6c9b0d5634c3393bc7e63c
gxsoar <2278926493gx@gmail.com>
2023-01-27 20:09:29 +0800

    init lab4

--- lock_protocol.h
+++ lock_protocol.h
@@ -25,4 +25,14 @@ public:
   lock_protocol::lockid_t lock_id_;
 };
 
+class rlock_protocol {
+ public:
+  enum xxstatus { OK, RPCERR };
+  typedef int status;
+  enum rpc_numbers {
+    revoke = 0x8001,
+    retry = 0x8002
+  };
+};
+
 #endif
---
+++ lock_server_cache.cc
@@ -0,0 +1,34 @@
+// the caching lock server implementation
+
+#include "lock_server_cache.h"
+
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sstream>
+
+#include "handle.h"
+#include "lang/verify.h"
+#include "tprintf.h"
+
+lock_server_cache::lock_server_cache() {}
+
+int lock_server_cache::acquire(lock_protocol::lockid_t lid, std::string id,
+                               int &) {
+  lock_protocol::status ret = lock_protocol::OK;
+  return ret;
+}
+
+int lock_server_cache::release(lock_protocol::lockid_t lid, std::string id,
+                               int &r) {
+  lock_protocol::status ret = lock_protocol::OK;
+  return ret;
+}
+
+lock_protocol::status lock_server_cache::stat(lock_protocol::lockid_t lid,
+                                              int &r) {
+  tprintf("stat request\n");
+  r = nacquire;
+  return lock_protocol::OK;
+}

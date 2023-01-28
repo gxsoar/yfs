85a5067a3c1109498887cd747cb1c882c00b162b
gxsoar <2278926493gx@gmail.com>
2023-01-28 16:50:10 +0800

    完成acquire

--- lock_client_cache.cc
+++ lock_client_cache.cc
@@ -26,11 +26,37 @@ lock_client_cache::lock_client_cache(std::string xdst,
 }
 
 lock_protocol::status lock_client_cache::acquire(lock_protocol::lockid_t lid) {
-  int ret = lock_protocol::OK;
-  return lock_protocol::OK;
+  std::unique_lock<std::mutex> ulock(mutex_);
+  // 先检查是否有其他线程持有该锁
+  if (lock_table_.count(lid)) {
+    while(lock_table_[lid]->getClientLockState() == ClientLockState::LOCKED) {
+      lock_table_[lid]->cv_.wait(ulock);
+    }
+    if (lock_table_[lid]->getClientLockState() == ClientLockState::FREE) {
+      lock_table_[lid]->setClientLockState(ClientLockState::LOCKED);
+      lock_table_[lid]->addThread(std::this_thread::get_id());
+      return lock_protocol::OK;
+    }
+  }
+  lock_table_[lid]->setClientLockState(ClientLockState::ACQUIRING);
+  int r;
+  auto ret = cl->call(lock_protocol::acquire, cl->id(), lid, r);
+  if (ret == lock_protocol::OK) {
+    lock_table_[lid]->setClientLockState(ClientLockState::LOCKED);
+    lock_table_[lid]->addThread(std::this_thread::get_id());
+  } else {
+    lock_table_[lid]->setClientLockState(ClientLockState::NONE);
+  }
+  return ret;
 }
 
 lock_protocol::status lock_client_cache::release(lock_protocol::lockid_t lid) {
+  if (!lock_table_.count(lid)) {
+    return lock_protocol::IOERR;
+  }
+  lock_table_[lid]->setClientLockState(ClientLockState::FREE);
+  // 唤醒所有等待的线程
+  lock_table_[lid]->cv_.notify_all();
   return lock_protocol::OK;
 }
 

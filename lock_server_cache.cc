747569f9644e7874bda98b7bbbe1fcdbcf910d7d
gxsoar <2278926493gx@gmail.com>
2023-01-29 17:07:14 +0800

    添加acquire

--- lock_server_cache.cc
+++ lock_server_cache.cc
@@ -16,13 +16,57 @@ lock_server_cache::lock_server_cache() {}
 
 int lock_server_cache::acquire(lock_protocol::lockid_t lid, std::string id,
                                int &) {
+  std::unique_lock<std::mutex> ulock(mutex_);
+  if (lock_table_.count(lid) == 0U) {
+    lock_table_[lid] = Lock(lid, ServerLockState::FREE);
+  }
+  auto &lock = lock_table_[lid];
   lock_protocol::status ret = lock_protocol::OK;
+  bool revoke = false;
+  if (lock.getServerLockState() == ServerLockState::FREE) {
+    lock.setServerLockState(ServerLockState::LOCKED);
+    lock.setLockOwner(id);
+    ret = lock_protocol::OK;
+  } else if (lock.getServerLockState() == ServerLockState::LOCKED) {
+    lock.setServerLockState(ServerLockState::LOCK_AND_WAIT);
+    revoke = true;
+    lock.addWaitClient(id);
+    ret = lock_protocol::RETRY;
+  } else if (lock.getServerLockState() == ServerLockState::LOCK_AND_WAIT) {
+    lock.addWaitClient(id);
+    ret = lock_protocol::RETRY;
+  } else {
+    if (lock.findWaitClient(id)) {
+      // 说明此时正在发送retry给该客户端，将其从集合中移除
+      lock.deleteWaitClient(id);
+      lock.setLockOwner(id);
+    } else {
+      if (lock.waitClientSetEmpty()) {
+        lock.setServerLockState(ServerLockState::LOCKED);
+        lock.setLockOwner(id);
+        ret = lock_protocol::OK;
+      } else {
+        lock.setServerLockState(ServerLockState::LOCK_AND_WAIT);
+        lock.addWaitClient(id);
+        revoke = true;
+        ret = lock_protocol::RETRY;
+      }
+    }
+  }
+  if (revoke) {
+    auto hold_client_id = lock.getLockOwner();
+    handle h(hold_client_id);
+    ulock.unlock();
+    int r;
+    h.safebind()->call(rlock_protocol::revoke, lid, r);
+  }
   return ret;
 }
 
 int lock_server_cache::release(lock_protocol::lockid_t lid, std::string id,
                                int &r) {
   lock_protocol::status ret = lock_protocol::OK;
+
   return ret;
 }
 

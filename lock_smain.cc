54e328df319c93ec29c0426dd9d24c0d78ea0c06
gxsoar <2278926493gx@gmail.com>
2023-01-30 23:18:17 +0800

    fix bug

--- lock_smain.cc
+++ lock_smain.cc
@@ -41,13 +41,13 @@ main(int argc, char *argv[])
   //jsl_set_debug(2);
 
 #ifndef RSM
-  lock_server ls;
+  // lock_server ls;
   rpcs server(atoi(argv[1]), count);
   lock_server_cache lsc;
-  server.reg(lock_protocol::stat, &ls, &lock_server::stat);
-  server.reg(lock_protocol::acquire, &ls, &lock_server::acquire);
-  server.reg(lock_protocol::release, &ls, &lock_server::release);
-  server.reg(lock_protocol::acquire, &lsc, &lock_server_cache::stat);
+  // server.reg(lock_protocol::stat, &ls, &lock_server::stat);
+  // server.reg(lock_protocol::acquire, &ls, &lock_server::acquire);
+  // server.reg(lock_protocol::release, &ls, &lock_server::release);
+  server.reg(lock_protocol::stat, &lsc, &lock_server_cache::stat);
   server.reg(lock_protocol::acquire, &lsc, &lock_server_cache::acquire);
   server.reg(lock_protocol::release, &lsc, &lock_server_cache::release);
 #endif

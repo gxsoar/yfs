71e33836a087f603dc370c71b6206e4af33cff2e
gxsoar <2278926493gx@gmail.com>
2023-02-16 18:28:51 +0800

    format log.cc

--- log.cc
+++ log.cc
@@ -1,28 +1,25 @@
-#include "paxos.h"
 #include <fstream>
 #include <iostream>
+
+#include "paxos.h"
 
 // Paxos must maintain some durable state (i.e., that survives power
 // failures) to run Paxos correct.  This module implements a log with
 // all durable state to run Paxos.  Since the values chosen correspond
 // to views, the log contains all views since the beginning of time.
 
-log::log(acceptor *_acc, std::string _me)
-  : pxs (_acc)
-{
+log::log(acceptor *_acc, std::string _me) : pxs(_acc) {
   name = "paxos-" + _me + ".log";
   logread();
 }
 
-void
-log::logread(void)
-{
+void log::logread(void) {
   std::ifstream from;
   std::string type;
   unsigned instance;
 
   from.open(name.c_str());
-  printf ("logread\n");
+  printf("logread\n");
   while (from >> type) {
     if (type == "done") {
       std::string v;
@@ -31,8 +28,8 @@ log::logread(void)
       getline(from, v);
       pxs->values[instance] = v;
       pxs->instance_h = instance;
-      printf ("logread: instance: %d w. v = %s\n", instance, 
-	      pxs->values[instance].c_str());
+      printf("logread: instance: %d w. v = %s\n", instance,
+             pxs->values[instance].c_str());
       pxs->v_a.clear();
       pxs->n_h.n = 0;
       pxs->n_a.n = 0;
@@ -47,19 +44,17 @@ log::logread(void)
       from.get();
       getline(from, v);
       pxs->v_a = v;
-      printf("logread: prop update %d(%s) with v = %s\n", pxs->n_a.n, 
-	     pxs->n_a.m.c_str(), pxs->v_a.c_str());
+      printf("logread: prop update %d(%s) with v = %s\n", pxs->n_a.n,
+             pxs->n_a.m.c_str(), pxs->v_a.c_str());
     } else {
       printf("logread: unknown log record\n");
       VERIFY(0);
     }
-  } 
+  }
   from.close();
 }
 
-std::string 
-log::dump()
-{
+std::string log::dump() {
   std::ifstream from;
   std::string res;
   std::string v;
@@ -71,9 +66,7 @@ log::dump()
   return res;
 }
 
-void
-log::restore(std::string s)
-{
+void log::restore(std::string s) {
   std::ofstream f;
   printf("restore: %s\n", s.c_str());
   f.open(name.c_str(), std::ios::trunc);
@@ -82,9 +75,7 @@ log::restore(std::string s)
 }
 
 // XXX should be an atomic operation
-void
-log::loginstance(unsigned instance, std::string v)
-{
+void log::loginstance(unsigned instance, std::string v) {
   std::ofstream f;
   f.open(name.c_str(), std::ios::app);
   f << "done";
@@ -98,9 +89,7 @@ log::loginstance(unsigned instance, std::string v)
 
 // an acceptor should call logprop(n_h) when it
 // receives a prepare to which it responds prepare_ok().
-void
-log::logprop(prop_t n_h)
-{
+void log::logprop(prop_t n_h) {
   std::ofstream f;
   f.open(name.c_str(), std::ios::app);
   f << "propseen";
@@ -114,9 +103,7 @@ log::logprop(prop_t n_h)
 
 // an acceptor should call logaccept(n_a, v_a) when it
 // receives an accept RPC to which it replies accept_ok().
-void
-log::logaccept(prop_t n, std::string v)
-{
+void log::logaccept(prop_t n, std::string v) {
   std::ofstream f;
   f.open(name.c_str(), std::ios::app);
   f << "accepted";
@@ -129,4 +116,3 @@ log::logaccept(prop_t n, std::string v)
   f << "\n";
   f.close();
 }
-
